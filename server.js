86cc3139aa4d71824ac868f83e8bdfe36aa3394a
Jorge Celaya <jcelaya775@gmail.com>
2022-01-02 21:29:46 -0600

    Version 1.0.0

--- server.js
+++ server.js
@@ -27,12 +27,15 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/pokemons', pokemons)
 
-// Serve assets to heroku
-// Set static folder
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 // Launch Server
 const port = process.env.PORT || 5000
