dfea53daf07d15064436628cc723a9887510a2e0
Jorge Celaya <jcelaya775@gmail.com>
2022-01-02 22:18:59 -0600

    commit

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
+        console.log('sending pokemons...');
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
