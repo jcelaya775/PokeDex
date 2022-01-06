1b3178b0d5877110324277a5f5882634fcfa2497
Jorge Celaya <jcelaya775@gmail.com>
2022-01-06 02:02:34 -0600

    further implementation

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,19 @@ function App() {
   const [query, setQuery] = useState('')
   const [pageNumber, setPageNumber] = useState(1)
 
+  const { mons, hasMore, loading, error } = useSearch(query, pageNumber)
+
   const observer = useRef()
+  const lastElementRef = useCallback((node) => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore)
+        setPageNumber(prev => prev + 1)
+    })
+    if (node) observer.current.observe(node)
+    console.log(node);
+  }, [loading, hasMore])
 
   const searchPokemon = async (s) => {
     s = s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
@@ -17,16 +29,11 @@ function App() {
     setPageNumber(1)
   }
 
-  const { mons, hasMore, loading, error } = useSearch(query, pageNumber)
-
-  // console.log('mons:')
-  // console.log(mons)
-
   return (
     <div>
       <Navbar onInput={searchPokemon} />
       {loading ? <LoadingSpinner className='h-fit w-fit flex justify-self-auto self-auto' /> :
-        mons && <Display pokemons={mons} />}
+        mons && <Display ref={lastElementRef} pokemons={mons} />}
     </div>
   )
 }
--- client/src/Display.js
+++ client/src/Display.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import Card from './Card'
 
-function Display({ pokemons }) {
+const Display = React.forwardRef(({ pokemons }, ref) => {
     return (
         <div className='bg-red-300'>
             <div className='container px-6 flex flex-wrap flex-grow justify-around items-center bg-blue-400'>
-                {pokemons.map((mon, key) => {
-                    return <Card pokemon={mon} key={key} />
+                {pokemons.map((mon, index) => {
+                    if (index == pokemons.length - 1)
+                        return <Card pokemon={mon} ref={ref} key={index} />
+
+                    return <Card pokemon={mon} key={index} />
                 })}
             </div>
         </div>
     )
-}
+})
 
 export default Display
